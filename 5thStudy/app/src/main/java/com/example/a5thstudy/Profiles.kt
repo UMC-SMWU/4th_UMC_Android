a5489d8f2533eb3e06a07a1fba1ef8cebd7148d0
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2023-05-03 17:07:33 +0900

    Delete 5thStudy directory

--- 5thStudy/app/src/main/java/com/example/a5thstudy/Profiles.kt
+++
@@ -1,5 +0,0 @@
-package com.example.a5thstudy
-
-class Profiles(val gender: Int, val name: String, val age: Int, val job: String, var isChecked: Boolean)
-
-//data class UserCheckStatus(val position: Int, var isChecked: Boolean)
